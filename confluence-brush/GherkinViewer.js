821c64c9b754c8cd85f94de025ed7a23efca53d7
John Byrne <johnbyrne.1984@gmail.com>
2015-11-25 13:07:47 +0000

    Create brush file for GherkinViewing
    initial commit

---
+++ confluence-brush/GherkinViewer.js
@@ -0,0 +1,6 @@
+ SyntaxHighlighter.brushes.Gherkin = function()
+{
+
+};
+SyntaxHighlighter.brushes.Gherkin.prototype = new SyntaxHighlighter.Highlighter();
+SyntaxHighlighter.brushes.Gherkin.aliases  = ['gherkin', 'cucumber', 'feature', 'features', 'scenario'];
